086135d15a72fecad74ad1ef7e8a8793fc8391b9
Gregory White <gregwhite90@gmail.com>
2023-09-25 11:32:46 -0400

    feat: add 2021-10 solution

---
+++ 2021/day-10/main.py
@@ -0,0 +1,36 @@
+import statistics
+from collections import deque
+
+OPENING_TO_CLOSING = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+}
+
+CLOSING_TO_POINTS = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
+
+if __name__ == '__main__':
+  incomplete_line_points = []
+  with open('input.txt') as f:
+    for line in f:
+      corrupted = False
+      expected_closings = deque()
+      for char in line.rstrip():
+        if char in OPENING_TO_CLOSING:
+          expected_closings.append(OPENING_TO_CLOSING[char])
+        elif expected_closings.pop() != char:
+          corrupted = True
+          break
+      if corrupted: continue
+      points = 0
+      while len(expected_closings) > 0:
+        points = points * 5 + CLOSING_TO_POINTS[expected_closings.pop()]
+      incomplete_line_points.append(points)
+        
+  print(statistics.median(incomplete_line_points))
