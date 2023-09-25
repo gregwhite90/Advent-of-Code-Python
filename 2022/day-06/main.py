1f859dd1963c52774dc624eb8554d4099c9fa999
Gregory White <gregwhite90@gmail.com>
2023-09-25 09:44:54 -0400

    feat: add 2022-06 solution

---
+++ 2022/day-06/main.py
@@ -0,0 +1,16 @@
+def first_marker(input: str, unique_chars: int = 4) -> int:
+  index = unique_chars
+  while len(set(input[index - unique_chars: index])) < unique_chars:
+    index += 1  
+  return index
+
+def parse_input(filename: str) -> str:
+  with open(filename) as f:
+    for l in f:
+      return l.rstrip()
+
+if __name__ == '__main__':
+  print(first_marker(
+    parse_input('input.txt'),
+    unique_chars = 14,
+  ))
