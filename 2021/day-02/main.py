5b7fca199c35cb15eedeeda5c541150a9e7b570e
Gregory White <gregwhite90@gmail.com>
2023-09-25 11:27:23 -0400

    feat: add 2021-02 solution

---
+++ 2021/day-02/main.py
@@ -0,0 +1,36 @@
+import csv
+
+def part_one():
+  with open('input.csv') as input_file:
+    input_reader = csv.reader(input_file)
+    x = 0
+    depth = 0
+    for line in input_reader:
+      directive = line[0].split(' ')
+      direction = directive[0]
+      distance = int(directive[1])
+      if direction == 'forward': x += distance
+      elif direction == 'up': depth -= distance
+      elif direction == 'down': depth += distance
+    return x * depth
+
+def part_two():
+  with open('input.csv') as input_file:
+    input_reader = csv.reader(input_file)
+    x = 0
+    depth = 0
+    aim = 0
+    for line in input_reader:
+      directive = line[0].split(' ')
+      direction = directive[0]
+      distance = int(directive[1])
+      if direction == 'forward':
+        x += distance
+        depth += aim * distance
+      elif direction == 'up': aim -= distance
+      elif direction == 'down': aim += distance
+    return x * depth
+
+if __name__ == '__main__':
+  print(part_one())
+  print(part_two())
