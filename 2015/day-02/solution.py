efe878ed5beb4d96ea7990dcca470ae772195002
Gregory White <gregwhite90@gmail.com>
2023-09-25 17:06:54 -0400

    feat: add 2015-02 solution

---
+++ 2015/day-02/solution.py
@@ -0,0 +1,47 @@
+import math
+from typing import Union, List
+
+from shared import SolutionABC
+
+class Box:
+  def __init__(self, row: str):
+    self.dimensions: List[int] = [int(dim) for dim in row.split('x')]
+    self.sides: List[int] = [
+      self.dimensions[0] * self.dimensions[1],
+      self.dimensions[0] * self.dimensions[2],
+      self.dimensions[1] * self.dimensions[2],
+    ]
+    self.perimeters: List[int] = [
+      2 * sum(self.dimensions[:2]),
+      2 * (self.dimensions[0] + self.dimensions[2]),
+      2 * sum(self.dimensions[1:]),
+    ]
+
+  def surface_area(self) -> int:
+    return 2 * sum(self.sides)
+
+  def slack(self) -> int:
+    return min(self.sides)
+
+  def volume(self) -> int:
+    return math.prod(self.dimensions)
+
+  def min_perimeter(self) -> int:
+    return min(self.perimeters)
+
+class Solution(SolutionABC):
+  def __init__(
+    self,
+  ):
+    self.boxes: List[Box] = []
+
+  def parse_row(
+    self,
+    row: str,
+  ):
+    self.boxes.append(Box(row))
+
+  def solve(
+    self,
+  ) -> Union[int, str]:
+    return sum(b.volume() + b.min_perimeter() for b in self.boxes)
