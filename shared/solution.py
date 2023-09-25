10017b082d1e577dd3f58b82b0f066a408c44426
Gregory White <gregwhite90@gmail.com>
2023-09-25 09:19:29 -0400

    feat: add shared functionality

---
+++ shared/solution.py
@@ -0,0 +1,19 @@
+from typing import Union
+from utils import SolutionABC
+
+class Solution(SolutionABC):
+  def __init__(
+    self,
+  ):
+    pass
+
+  def parse_row(
+    self,
+    row: str,
+  ):
+    pass
+
+  def solve(
+    self,
+  ) -> Union[int, str]:
+    pass
